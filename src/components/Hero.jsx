19d3197235a2911720082806b762d4c91ba05745
galurralde <gustavo.alurralde@gmail.com>
2026-06-10 21:42:52 -0300

    Integracion boton AR

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,29 @@
 export default function Hero() {
   return (
     <main className="hero">
-
       <div className="hero-content">
-
         <span className="hero-badge">
           Turismo Cultural + Realidad Aumentada
         </span>
 
         <h2>
-          Descubrí la historia del Pueblo Tolombón mediante experiencias inmersivas
+          Descubrí la historia del Pueblo Tolombón mediante experiencias
+          inmersivas
         </h2>
 
         <p>
-          Un recorrido interactivo por las Guerras Calchaquíes,
-          la memoria territorial y el patrimonio cultural del Valle de Yocavil.
+          Un recorrido interactivo por las Guerras Calchaquíes, la memoria
+          territorial y el patrimonio cultural del Valle de Yocavil.
         </p>
 
         <div className="hero-buttons">
-          <button>
-            Iniciar Experiencia AR
+          <button onClick={() => window.open("/ar/index.html", "_blank")}>
+            📷 Abrir Cámara AR
           </button>
 
-          <button className="btn-secondary">
-            Conocer el Proyecto
-          </button>
+          <button className="btn-secondary">Conocer el Proyecto</button>
         </div>
-
       </div>
-
     </main>
   );
 }
